06fbe69431165b3c7b0fcf84b71b374d7b54368f
IgorMushk <igor.mushketik@gmail.com>
2023-09-22 11:49:44 +0300

    Upd refactoring ImageGallery class

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,38 +1,69 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import css from './ImageGallery.module.css';
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
 import Modal from 'components/Modal/Modal';
 
-export default class ImageGallery extends Component {
-  state = {
-    largeImageURL: '',
-    showModal: false,
-  };
-openModal = imgURL => {
-    this.setState({largeImageURL: imgURL, showModal: true})
+export default function ImageGallery({data}) {
+  const [largeImageURL, setLargeImageURL] = useState('');
+  const [showModal, setShowModal] = useState('');
+
+const openModal = imgURL => {
+  setLargeImageURL(imgURL);
+  setShowModal(true);
 };
 
-closeModal = () => {
-    this.setState({largeImageURL: '', showModal: false})
+const closeModal = () => {
+  setLargeImageURL('');
+  setShowModal(false);
 }
 
-render() {
-    const { data } = this.props;
-    //console.log('data', data)
-    return (
-        <>
+  return (
+    <>
       <ul className={css.ImageGallery}>
         {data.map(img => (
-          <ImageGalleryItem 
-          key={img.id} 
-          webformatURL={img.webformatURL} 
-          onOpenModal={()=>this.openModal(img.largeImageURL)}
+          <ImageGalleryItem
+            key={img.id}
+            webformatURL={img.webformatURL}
+            onOpenModal={() => openModal(img.largeImageURL)}
           />
         ))}
-        { this.state.showModal && (<Modal img={this.state.largeImageURL} onClose={this.closeModal}/>)}
+        {showModal && (
+          <Modal img={largeImageURL} onClose={closeModal} />
+        )}
       </ul>
-      </>
-    );
-  }
+    </>
+  );
 }
 
+// export default class ImageGallery extends Component {
+//   state = {
+//     largeImageURL: '',
+//     showModal: false,
+//   };
+// openModal = imgURL => {
+//     this.setState({largeImageURL: imgURL, showModal: true})
+// };
+
+// closeModal = () => {
+//     this.setState({largeImageURL: '', showModal: false})
+// }
+
+// render() {
+//     const { data } = this.props;
+//     //console.log('data', data)
+//     return (
+//         <>
+//       <ul className={css.ImageGallery}>
+//         {data.map(img => (
+//           <ImageGalleryItem
+//           key={img.id}
+//           webformatURL={img.webformatURL}
+//           onOpenModal={()=>this.openModal(img.largeImageURL)}
+//           />
+//         ))}
+//         { this.state.showModal && (<Modal img={this.state.largeImageURL} onClose={this.closeModal}/>)}
+//       </ul>
+//       </>
+//     );
+//   }
+// }
