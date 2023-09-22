2626ee6957ee0590005f337862ccc25c764e2357
IgorMushk <igor.mushketik@gmail.com>
2023-09-22 11:55:59 +0300

    Upd  ImageGallery function working - del comments prev class

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -35,35 +35,3 @@ const closeModal = () => {
   );
 }
 
-// export default class ImageGallery extends Component {
-//   state = {
-//     largeImageURL: '',
-//     showModal: false,
-//   };
-// openModal = imgURL => {
-//     this.setState({largeImageURL: imgURL, showModal: true})
-// };
-
-// closeModal = () => {
-//     this.setState({largeImageURL: '', showModal: false})
-// }
-
-// render() {
-//     const { data } = this.props;
-//     //console.log('data', data)
-//     return (
-//         <>
-//       <ul className={css.ImageGallery}>
-//         {data.map(img => (
-//           <ImageGalleryItem
-//           key={img.id}
-//           webformatURL={img.webformatURL}
-//           onOpenModal={()=>this.openModal(img.largeImageURL)}
-//           />
-//         ))}
-//         { this.state.showModal && (<Modal img={this.state.largeImageURL} onClose={this.closeModal}/>)}
-//       </ul>
-//       </>
-//     );
-//   }
-// }
