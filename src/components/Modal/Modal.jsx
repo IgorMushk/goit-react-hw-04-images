1caeb99615da7bfd21b1375b9de04400da6dcb27
IgorMushk <igor.mushketik@gmail.com>
2023-09-22 11:22:36 +0300

    del comments - prev class Modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,25 +1,22 @@
 //import React, { Component } from 'react';
-import css from './Modal.module.css';
-
 import React, { useEffect } from 'react';
+import css from './Modal.module.css';
 
 export default function Modal({ img, onClose }) {
   //  componentDidMount
   //  componentWillUnmount
   useEffect(() => {
-    console.log('set handleKeyDown');
-    //  handleKeyDown
+    //console.log('set handleKeyDown');
     const handleKeyDown = evt => {
       if (evt.code === 'Escape') onClose();
     };
     window.addEventListener('keydown', handleKeyDown);
     return () => {
-      console.log('remove handleKeyDown ');
+      //console.log('remove handleKeyDown ');
       window.removeEventListener('keydown', handleKeyDown);
     };
   }, [onClose]);
 
-  //  handleClickBackdrop
   const handleClickBackdrop = evt => {
     if (evt.target === evt.currentTarget) onClose();
   };
@@ -32,36 +29,3 @@ export default function Modal({ img, onClose }) {
     </div>
   );
 }
-
-// export default class Modal extends Component {
-//   componentDidMount() {
-//     window.addEventListener('keydown', this.handleKeyDown);
-//   }
-
-//   componentWillUnmount() {
-//     window.removeEventListener('keydown', this.handleKeyDown);
-//   }
-
-//   handleKeyDown = evt => {
-//     if (evt.code === 'Escape') {
-//       this.props.onClose();
-//     }
-//   };
-
-//   handleClickBackdrop = evt => {
-//     if (evt.target === evt.currentTarget) {
-//       this.props.onClose();
-//     }
-//   };
-
-//   render() {
-//     const { img } = this.props;
-//     return (
-//       <div className={css.Overlay} onClick={this.handleClickBackdrop}>
-//         <div className={css.Modal}>
-//           <img src={img} alt="" />
-//         </div>
-//       </div>
-//     );
-//   }
-// }
