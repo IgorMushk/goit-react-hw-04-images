82d7f9db588347eb26cca9a455766472d60a38b1
IgorMushk <igor.mushketik@gmail.com>
2023-09-22 11:10:49 +0300

    Upd  - refactoring class Modal to Hooks

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,35 +1,67 @@
-import React, { Component } from 'react';
+//import React, { Component } from 'react';
 import css from './Modal.module.css';
 
-export default class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+import React, { useEffect } from 'react';
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
+export default function Modal({ img, onClose }) {
+  //  componentDidMount
+  //  componentWillUnmount
+  useEffect(()=>{
+    console.log('set handleKeyDown');
+    window.addEventListener('keydown', handleKeyDown);
+    return ()=>{
+      console.log('remove handleKeyDown ');
+      window.removeEventListener('keydown', handleKeyDown)}
+  },[]);
 
-  handleKeyDown = evt => {
-    if (evt.code === 'Escape') {
-      this.props.onClose();
-    }
+  //  handleKeyDown
+  const handleKeyDown = evt => {
+    if (evt.code === 'Escape') onClose();
   };
 
-  handleClickBackdrop = evt => {
-    if (evt.target === evt.currentTarget) {
-      this.props.onClose();
-    }
+  //  handleClickBackdrop
+  const handleClickBackdrop = evt => {
+    if (evt.target === evt.currentTarget) onClose();
   };
 
-  render() {
-    const { img } = this.props;
-    return (
-      <div className={css.Overlay} onClick={this.handleClickBackdrop}>
-        <div className={css.Modal}>
-          <img src={img} alt="" />
-        </div>
+  return (
+    <div className={css.Overlay} onClick={handleClickBackdrop}>
+      <div className={css.Modal}>
+        <img src={img} alt="" />
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+// export default class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   handleKeyDown = evt => {
+//     if (evt.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+
+//   handleClickBackdrop = evt => {
+//     if (evt.target === evt.currentTarget) {
+//       this.props.onClose();
+//     }
+//   };
+
+//   render() {
+//     const { img } = this.props;
+//     return (
+//       <div className={css.Overlay} onClick={this.handleClickBackdrop}>
+//         <div className={css.Modal}>
+//           <img src={img} alt="" />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
